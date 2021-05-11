74621d0368f0a0aeb869ba3db97ae97340d255ec
mishail <WhiteBear05@yandex.ru>
2021-05-11 20:30:06 +0400

    -

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from db import db, User
 from dialogflow_request import request
 import gamedata as gd
 
+gdlist = {}
+
 token_f = open('token', 'r')
 token = token_f.read()
 
@@ -34,10 +36,12 @@ async def on_message(message):
 
     user = User.query.filter_by(user_id=message.author.id).first()
     if user == None:
+        print('\n---\nNew user detected: {}\nAdding to db...'.format(str(message.author.id)))
         new_user = User(user_id=message.author.id, user_func='start', username=message.author.display_name, user_was_shown=False, user_attempts=1)
         db.session.add(new_user)
         db.session.commit()
         user = User.query.filter_by(user_id=message.author.id).first()
+        print('Done!\n---\n')
 
     if msg.startswith('!help'):
         embed = discord.Embed(title='Основные команды')
@@ -47,69 +51,101 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
     elif msg.startswith('!игнат'):
-        await message.channel.send('`{}`'.format(request(message.author.id, cut_cmd(msg))))
+        print('\n---\nNew df request\nUser: {}\nMessage: {}'.format(str(message.author.id), cut_cmd(msg)))
+        response = request(message.author.id, cut_cmd(msg))
+        await message.channel.send('`{}`'.format(response))
+        print('Response: {}\n---\n'.format(response))
 
     elif msg.startswith('!start'):
+        print('\n---\nProgress drop\nUser: {}\n---\n'.format(message.author.id))
         user.user_func = 'start'
+        user.user_was_shown = False
         db.session.commit()
+        try:
+            del gdlist[str(message.author.id)]
+        except Exception:
+            pass
         await message.channel.send('Прогресс был сброшен!')
 
     else:
-        game = gd.GameData()
-        func = getattr(game, user.user_func)
-        wasShown = user.user_was_shown
+        try:
+            game = gdlist[str(message.author.id)]
+        except Exception as e:
+            print('\n---\nNew user connected: {}\n---\n'.format(str(message.author.id)))
+            gdlist[str(message.author.id)] = gd.UserObject
+            game = gdlist[str(message.author.id)]
 
-        if wasShown and gd.selection_list == []:
+        func = getattr(game.GameData(), user.user_func)
+
+        wasShown = user.user_was_shown
+        if wasShown and game.selection_list == []:
             wasShown = False
 
         if wasShown:
             try:
-                if gd.selection_list != ['next']:
+                if game.selection_list != ['next']:
                     choice = int(msg)
                 else:
                     choice = 0
-                if 1 <= choice <= len(gd.selection_list) or gd.selection_list == ['next']:
-                    gd.embed_send_list, gd.send_list = [], []
+                if 1 <= choice <= len(game.selection_list) or game.selection_list == ['next']:
+                    game.embed_send_list, game.send_list = [], []
+
                     func(choice)
-                    for i in gd.embed_send_list:
-                        await message.channel.send(embed=i)
-                    for i in gd.send_list:
+
+                    for i in game.embed_send_list:
+                        if type(i) is str:
+                            await message.channel.send(i)
+                        else:
+                            await message.channel.send(embed=i)
+                    for i in game.send_list:
                         await message.channel.send(i)
-                    user.user_func = gd.next_func
+                    user.user_func = game.next_func
                     user.user_was_shown = False
                     db.session.commit()
 
-                    func = getattr(game, gd.next_func)
-                    gd.embed_send_list, gd.send_list, gd.selection_list = [], [], []
+                    func = getattr(game.GameData(), game().next_func)
+                    game.embed_send_list, game.send_list, game.selection_list = [], [], []
+
                     func(None)
-                    for i in gd.embed_send_list:
-                        await message.channel.send(embed=i)
-                    if gd.selection_list != ['next']:
-                        for i in gd.selection_list:
+
+                    for i in game.embed_send_list:
+                        if type(i) is str:
+                            await message.channel.send(i)
+                        else:
+                            await message.channel.send(embed=i)
+                    if game.selection_list != ['next']:
+                        for i in game.selection_list:
                             await message.channel.send(i)
                     else:
                         await message.channel.send('Отправьте любое сообщение, чтобы продолжить.')
-                    for i in gd.send_list:
+                    for i in game.send_list:
                         await message.channel.send(i)
 
                     user.user_was_shown = True
                     db.session.commit()
                 else:
-                    await message.channel.send('Ввод должен являться числом от 1 до {}!'.format(len(gd.selection_list)))
+                    await message.channel.send('Ввод должен являться числом от 1 до {}!'.format(len(game.selection_list)))
             except Exception as e:
+                print(e)
                 await message.channel.send('Ввод должен являться целым числом!')
 
         if not wasShown:
+
+            game.embed_send_list, game.send_list, game.selection_list = [], [], []
+
             func(None)
 
-            for i in gd.embed_send_list:
-                await message.channel.send(embed=i)
-            if gd.selection_list != ['next']:
-                for i in gd.selection_list:
+            for i in game.embed_send_list:
+                if type(i) is str:
+                    await message.channel.send(i)
+                else:
+                    await message.channel.send(embed=i)
+            if game.selection_list != ['next']:
+                for i in game.selection_list:
                     await message.channel.send(i)
             else:
                 await message.channel.send('Отправьте любое сообщение, чтобы продолжить.')
-            for i in gd.send_list:
+            for i in game.send_list:
                 await message.channel.send(i)
 
             user.user_was_shown = True
