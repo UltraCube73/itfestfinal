11b52b41ff1cc1b82704f6918b1ca2355ea6dfc4
mishail <WhiteBear05@yandex.ru>
2021-05-12 12:41:25 +0400

    -

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from db import db, User
 from dialogflow_request import request
 import gamedata as gd
+import function_table as ft
 
 gdlist = {}
 
@@ -44,16 +45,48 @@ async def on_message(message):
         print('Done!\n---\n')
 
     if msg.startswith('!help'):
-        embed = discord.Embed(title='Основные команды')
+        embed = discord.Embed(title='Список команд')
         embed.add_field(name='!игнат <сообщение>', value='Обращение к умному ассистенту')
+        embed.add_field(name='!rating', value='Простотр топа игроков и информации о Вас')
         embed.add_field(name='!help', value='Просмотр списка команд')
         await message.channel.send(embed=embed)
 
     elif msg.startswith('!игнат'):
         print('\n---\nNew df request\nUser: {}\nMessage: {}'.format(str(message.author.id), cut_cmd(msg)))
-        response = request(message.author.id, cut_cmd(msg))
-        await message.channel.send('`{}`'.format(response))
-        print('Response: {}\n---\n'.format(response))
+        if cut_cmd(msg) == '':
+            await message.channel.send('После команды должен следовать текст обращения! Например:"!игнат число пи".')
+            print('Empty message.\n---\n')
+        else:
+            response = request(message.author.id, cut_cmd(msg))
+            await message.channel.send('`{}`'.format(response))
+            print('Response: {}\n---\n'.format(response))
+
+    elif msg.startswith('!rating'):
+        print('\n---\nRating\nUser: {}\n---\n'.format(message.author.id))
+        userlist = User.query.all()
+        users_in_rating = []
+        for i in userlist:
+            user_in_rating = dict()
+            user_in_rating['id'] = i.user_id
+            user_in_rating['nickname'] = i.username
+            user_in_rating['score'] = ft.score_table[i.user_func] - i.user_attempts
+            users_in_rating.append(user_in_rating)
+        users_in_rating = sorted(users_in_rating, key=lambda user_in_rating: user_in_rating['score'], reverse=True)
+        for i in range(len(users_in_rating)):
+            if users_in_rating[i]['id'] == user.user_id:
+                place_in_rating = i+1
+                break
+        embed = discord.Embed(title='Топ-3 игроков')
+        embed.add_field(name='1е место:', value=users_in_rating[0]['nickname'])
+        embed.add_field(name='2е место:', value=users_in_rating[1]['nickname'])
+        embed.add_field(name='3е место:', value=users_in_rating[2]['nickname'])
+        await message.channel.send(embed=embed)
+        embed = discord.Embed(title='О Вас')
+        embed.add_field(name='Ваше место:', value=place_in_rating)
+        embed.add_field(name='Этап прохождения:', value=ft.function_table[user.user_func])
+        embed.add_field(name='Попытки:', value=user.user_attempts)
+        await message.channel.send(embed=embed)
+        await message.channel.send('Ссылка на полный рейтинг: https://quest-ratings.herokuapp.com/index')
 
     elif msg.startswith('!'):
         await message.channel.send('Неизвестная команда!')
@@ -116,8 +149,11 @@ async def on_message(message):
                     db.session.commit()
                 else:
                     await message.channel.send('Ввод должен являться числом от 1 до {}!'.format(len(game.selection_list)))
-            except Exception as e:
+            except ValueError:
                 await message.channel.send('Ввод должен являться целым числом!')
+            except Exception as e:
+                print(e)
+                await message.channel.send('Сбой программы. Обратитесь к администратору за помощью.')
 
         if not wasShown:
 
